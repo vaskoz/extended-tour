d37751c77f8f75c7d9d17cea58c1534ec29b4b36
Vasko Zdravevski <vaskoz@users.noreply.github.com>
2017-09-19 11:52:04 -0600

    switch graceful shutdown to context.Context.WithCancel

--- content/microservice/graceful.go
+++ content/microservice/graceful.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"os/signal"
@@ -9,32 +10,38 @@ import (
 )
 
 type Application struct {
-	notifyWhenDone chan struct{}
+	ctx context.Context
 }
 
-func (app *Application) Run() <-chan struct{} {
+func (app *Application) Run(ctx context.Context) <-chan struct{} {
+	app.ctx = ctx
 	go func() {
-		time.Sleep(30 * time.Second)
-		app.Stop()
+		for i := 0; i < 30; i++ {
+			time.Sleep(1 * time.Second) // do some simulated work
+			select {
+			case <-ctx.Done():
+				break
+			default:
+				log.Println("Completed iteration", i)
+			}
+		}
 	}()
-	return app.notifyWhenDone
-}
-
-func (app *Application) Stop() {
-	app.notifyWhenDone <- struct{}{}
+	return ctx.Done()
 }
 
 func main() {
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 
-	application := &Application{make(chan struct{}, 1)}
+	application := &Application{}
+	ctx, cancel := context.WithCancel(context.Background())
 
 	select {
-	case <-application.Run():
+	case <-application.Run(ctx):
 		log.Println("I've completed my work")
 	case <-sigChan:
-		application.Stop()
+		cancel()
+		<-ctx.Done()
 		log.Println("I was asked to stop")
 	}
 }
